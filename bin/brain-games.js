a1799b400c89bfc6e0701bb8decf0cd06be300c6
YuriyNotPickle <yurriyklimenko@gmail.com>
2025-01-30 23:45:27 +0300

    Шаг 9

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 import { getName } from '../src/cli.js';
-import brainEven from './brain-even.js';
 
 console.log('Welcome to the Brain Games!');
 const name = getName();
 console.log(`Hello, ${name}!`);
-brainEven(0);
-console.log(`Congratulations, ${name}!`);
